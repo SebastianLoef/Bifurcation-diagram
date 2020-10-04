e13b3d7974eda958898cb77c4807c67f9ede63d6
SebastianLoef <sebastian@loef.se>
2020-10-04 22:16:42 +0200

    works now

--- generate_diagram.py
+++ generate_diagram.py
@@ -1,23 +1,27 @@
-import collections
-from bigfloat import mul, sub, precision
+import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-def logistic_map(x0, r, iterations, length):
-    x = collections.deque([x0], maxlen=length)
-    for i in range(iterations):
-        x.append(r*x[-1]*(1-x[-1]))
-    return x
 
+WIDTH = 3840*2
+HEIGHT = 2160*2
+img = np.ones((HEIGHT, WIDTH, 4))
+BG_COLOR = np.array([33, 37, 41, 1])
+img *= BG_COLOR
+img[:, :, 3] = 255
+# Some asthetic gaussian noise
+img[:, :, :3] += (3*np.random.randn(HEIGHT, WIDTH, 3)).astype(int)
 
-fig, ax = plt.subplots()
-
-R = np.linspace(0, 10, 101)
+R = np.linspace(2, 4, 10001)
 N = 100
-x = 0.000001
 for r in tqdm(R):
-    X = logistic_map(x, r, 5*N, N)
-    ax.scatter([r]*N, X, marker='.', color='red', linewidth=0.01)
-    
-fig.savefig('fig.png', dpi=300)
+    x = 0.000001
+    for i in range(10*N):
+        if i >= 8*N:
+            coordinate = (int((r-2)/2*WIDTH), int((1-x)*HEIGHT))
+            img = cv2.circle(img, coordinate, 1, color=(53, 191, 92, 200),
+                             thickness=-1)
+        x = r*x*(1-x)
+
+img = cv2.blur(img, (3, 3))
+cv2.imwrite('test.png', img)
